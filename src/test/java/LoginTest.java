64607fdb40f4e814fe7d238b3ba767f3f23ece05
pathaksarita2014 <pathaksarita2014@gmail.com>
2022-01-17 11:14:33 -0500

    added all files

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class LoginTest
+{
+    @Test
+    public void login()
+    {
+        //webdriver.chrome.driver
+
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") +
+                "//src//test//resources//bina//" + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/");
+        Assert.assertTrue(true);
+    }
+}

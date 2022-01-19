5a6d886a95430fdc40efa5edf8cf6350495b0aae
pathaksarita2014 <pathaksarita2014@gmail.com>
2022-01-19 09:13:46 -0500

    exe

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -14,9 +14,12 @@ public class LoginTest
         //webdriver.chrome.driver
         ChromeOptions option = new ChromeOptions();
         option.addArguments("headless");
-       // System.setProperty("webdriver.chrome.driver","src/test/resources/newbinary/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver","/src/test/resources/linux/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        /*System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//newbinary//"
+                +"chromedriver.exe");*/
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
+                                                                                      +"chromedriver");
+
+        WebDriver driver = new ChromeDriver(option);
         driver.get("https://www.youtube.com/");
         System.out.println("Test Success");
         Assert.assertTrue(true);

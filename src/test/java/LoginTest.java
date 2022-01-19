d4fd54557ffff15ebd18b988733453e3e49f7e3a
Sarita Pathak <spathak.ext@goeasy.com>
2022-01-19 13:47:58 -0500

    aa

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.sql.Driver;
 
 public class LoginTest
 {
@@ -12,17 +11,14 @@ public class LoginTest
     public void login()
     {
         //webdriver.chrome.driver
+
         ChromeOptions option = new ChromeOptions();
-        option.addArguments("headless");
-        option.setBinary(System.getProperty("user.dir")+ "//src//test//resources//linux//"
-                +"chromedriver");
-     /* System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//newbinary//"
-                +"chromedriver.exe");*/
+        option.setHeadless(true);
+        option.setBinary(System.getProperty("user.dir") + "//src//test//resources//testexe//chrome.exe");
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
                 +"chromedriver");
         WebDriver driver = new ChromeDriver(option);
         driver.get("https://www.youtube.com/");
-        System.out.println("Test Success");
         Assert.assertTrue(true);
     }
 }

fd979b747d2a9bf5cd2c61ec635687edc6f0b4fd
Sarita Pathak <spathak.ext@goeasy.com>
2022-01-19 15:09:51 -0500

    aa

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -12,22 +12,16 @@ public class LoginTest
     @Test
     public void login()
     {
-        //webdriver.chrome.driver
-
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
+                +"chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
         options.addArguments("headless");
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-
-
         options.setBinary(System.getProperty("user.dir") + "//src//test//resources//testexe//chrome.exe");
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
-                +"chromedriver");
+
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.youtube.com/");
         Assert.assertTrue(true);

023a80e6f6812aa2fe804be928285faba0b9f874
Sarita Pathak <spathak.ext@goeasy.com>
2022-01-19 13:27:52 -0500

    aa

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -17,7 +17,7 @@ public class LoginTest
         ChromeOptions option = new ChromeOptions();
 
         option.addArguments("headless");
-        option.setBinary("//src//test//resources//testexe//chrome.exe");
+        option.setBinary(System.getProperty("user.dir") + "//src//test//resources//testexe//chrome.exe");
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
                 +"chromedriver");
         WebDriver driver = new ChromeDriver(option);

ef312be9a8def3fb134c29e45342d3edb688cc5a
Sarita Pathak <spathak.ext@goeasy.com>
2022-01-19 13:25:41 -0500

    aa

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 public class LoginTest
 {
@@ -10,9 +13,11 @@ public class LoginTest
     public void login()
     {
         //webdriver.chrome.driver
+
         ChromeOptions option = new ChromeOptions();
+
         option.addArguments("headless");
-        option.setBinary(System.getProperty("user.dir")+ "//src//test//resources//testexe//chrome.exe");
+        option.setBinary("//src//test//resources//testexe//chrome.exe");
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
                 +"chromedriver");
         WebDriver driver = new ChromeDriver(option);

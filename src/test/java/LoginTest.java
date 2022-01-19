d185bfdba0cf8f024a7c79454fb9017cde3d2e0c
Sarita Pathak <spathak.ext@goeasy.com>
2022-01-19 11:12:16 -0500

    added binary

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.sql.Driver;
-
 public class LoginTest
 {
     @Test
@@ -14,11 +12,8 @@ public class LoginTest
         //webdriver.chrome.driver
         ChromeOptions option = new ChromeOptions();
         option.addArguments("headless");
-        option.setBinary(System.getProperty("user.dir")+ "//src//test//resources//linux//"
-                +"chromedriver");
-     /* System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//newbinary//"
-                +"chromedriver.exe");*/
-       System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
+        option.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
                 +"chromedriver");
         WebDriver driver = new ChromeDriver(option);
         driver.get("https://www.youtube.com/");

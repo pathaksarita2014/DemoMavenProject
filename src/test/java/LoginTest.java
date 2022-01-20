e2398ff7a066cae380a286e3cc19e20a2ddc67f5
Sarita Pathak <spathak.ext@goeasy.com>
2022-01-20 09:33:35 -0500

    exe

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -12,15 +12,14 @@ public class LoginTest
     @Test
     public void login()
     {
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//linux//"
-                +"chromedriver");
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "//src//test//resources//newbinary//"
+                +"chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.setHeadless(true);
-        options.addArguments("--remote-debugging-port=3647");
         options.setBinary(System.getProperty("user.dir") + "//src//test//resources//testexe//chrome.exe");
 
         WebDriver driver = new ChromeDriver(options);
